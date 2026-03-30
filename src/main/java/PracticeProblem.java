9a465b16f1b416b3b558b7655268f98dd8997dfc
NarenNades <344941885@tdsb.ca>
2026-03-30 15:39:46 +0000

    Done!

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,31 @@
+/**
+ * Lesson: Problem Solving and Good Programming Style
+ * Author: Naren Nades
+ * Date Created: Mar 30, 2026
+ * Date Last Modified: Mar 30, 2026
+ */
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	public static void q1() {
+	public static String getFirstName(String name) {
 		//Write question 1 code here
+		String firstName = name.trim().substring(0, name.trim().indexOf(" "));
+		return firstName;
 	}
 
-	public static void q2() {
+	public static String getLastName(String name) {
 		//Write question 2 code here
+		String lastName = name.trim().substring(name.trim().lastIndexOf(" ")+1);
+		return lastName;
 	}
 
-	public static void q3() {
+	public static Boolean isValidName(String name) {
 		//Write question 3 code here
+		Boolean validName = name.trim().contains(" ") && name.trim().substring(0,name.trim().indexOf(" ")).length()>=2 && name.trim().substring(name.trim().lastIndexOf(" ")+1).length()>=2 && !name.trim().replaceFirst(" ", "").contains(" ");
+		return validName;
 	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
 }
--- src/test/java/PracticeProblemTest.java
+++ src/test/java/PracticeProblemTest.java
@@ -1,9 +1,10 @@
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.lang.reflect.Method;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class PracticeProblemTest {
 
